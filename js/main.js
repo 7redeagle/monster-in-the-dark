b107e6990966e7401b46c86ae2ba74d8548f34b0
tiger-fish <25111698+tiger+fish@users.noreply.github.com>
2020-10-20 16:30:01 -0400

    setup welcome screen

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// global variables
+
+enterGame = () => {
+  let welcomeButton = document.querySelector(".welcome__button");
+  let welcome = document.querySelector(".welcome");
+  let html = document.querySelector("html");
+  welcomeButton.onclick = () => {
+    welcome.style.visibility = "hidden";
+    html.style.visibility = "visible";
+  };
+};
+
+enterGame();
+
+// flashlight cursor
+function update(e) {
+  var x = e.clientX || e.touches[0].clientX;
+  var y = e.clientY || e.touches[0].clientY;
+
+  document.documentElement.style.setProperty("--cursorX", x + "px");
+  document.documentElement.style.setProperty("--cursorY", y + "px");
+}
+
+document.addEventListener("mousemove", update);
+document.addEventListener("touchmove", update);
