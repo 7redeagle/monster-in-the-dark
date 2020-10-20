c73b46e62bc4a4da42c1754e50857eae1c7262ce
tiger-fish <25111698+tiger+fish@users.noreply.github.com>
2020-10-20 17:28:49 -0400

    light shrink function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// global variables
-
-enterGame = () => {
-  let welcomeButton = document.querySelector(".welcome__button");
-  let welcome = document.querySelector(".welcome");
-  let html = document.querySelector("html");
-  welcomeButton.onclick = () => {
-    welcome.style.visibility = "hidden";
-    html.style.visibility = "visible";
-  };
-};
-
-enterGame();
-
 // flashlight cursor
 function update(e) {
   var x = e.clientX || e.touches[0].clientX;
@@ -24,12 +10,30 @@ function update(e) {
 document.addEventListener("mousemove", update);
 document.addEventListener("touchmove", update);
 
-// game
+let welcomeButton = document.querySelector(".welcome__button");
+let welcome = document.querySelector(".welcome");
+let html = document.querySelector("html");
 let monster = document.querySelector(".monster");
 
-monster.onmouseover = () => {
-  alert("You vanquished the monster");
-  monster.style.cssText("transform: scale(0)");
+enterGame = () => {
+  welcomeButton.onclick = () => {
+    welcome.style.visibility = "hidden";
+    html.style.visibility = "visible";
+  };
+
+  setTimeout(function () {
+    document.documentElement.style.cssText = "--cursorSize: 6vmax";
+    html.style.visibility = "visible";
+    monster.style.top = Math.floor(Math.random() * 50);
+    monster.style.left = Math.floor(Math.random() * 50);
+  }, 3000);
 };
 
+enterGame();
 
+// game
+
+monster.onmouseover = () => {
+  // alert("You vanquished the monster");
+  monster.style.cssText = "transform: scale(1)";
+};
