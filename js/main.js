6246ddb201c3894041b29c07706cd3dad15803d4
tiger-fish <25111698+tiger+fish@users.noreply.github.com>
2020-10-20 20:49:16 -0400

    added monster set interval of 5s

--- js/main.js
+++ js/main.js
@@ -10,28 +10,46 @@ function update(e) {
 document.addEventListener("mousemove", update);
 document.addEventListener("touchmove", update);
 
-// click button to begin game
+// game setup
 const allBtn = document.querySelectorAll(".button");
 const mainScreen = document.querySelector(".main");
 const gameScreen = document.querySelector("html");
 const monster = document.querySelector(".monster");
 
+// move monster to random position
+moveMonster = () => {
+  function getRandomPosition(e) {
+    var x = document.body.offsetHeight - e.clientHeight;
+    var y = document.body.offsetWidth - e.clientWidth;
+    var randomX = Math.floor(Math.random() * x);
+    var randomY = Math.floor(Math.random() * y);
+    return [randomX, randomY];
+  }
+  var xy = getRandomPosition(monster);
+  monster.style.top = xy[0] + "px";
+  monster.style.left = xy[1] + "px";
+};
+
+// The monster will move every 5 seconds
+let intervalMonster = window.setInterval(moveMonster, 5000);
+
+// click button to begin game and hide the mainScreen. Each button links to different background images.
 allBtn.forEach(function (button) {
   button.onclick = () => {
     mainScreen.style.visibility = "hidden";
     gameScreen.style.visibility = "visible";
+    moveMonster();
     if (button.classList.contains("button__hard")) {
       gameScreen.style.backgroundImage = "url(assets/hard.jpg)";
     } else if (button.classList.contains("button__medium")) {
       gameScreen.style.backgroundImage = "url(assets/medium.jpg)";
     } else {
       gameScreen.style.backgroundImage = "url(assets/easy.jpg)";
+      moveMonster();
     }
   };
 });
 
 // music: auto play music once one of the buttons are clicked, play different music when game lost or won (royalty free music)
-
-// fix monster random not working
 
 // use functions for items that are reusable
