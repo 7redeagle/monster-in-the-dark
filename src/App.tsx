c0383fc4f2ef27615f168ec359136876cb9f8abf
7redeagle <25111698+7redeagle@users.noreply.github.com>
2021-06-19 18:48:17 -0400

    added Game component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Game from './components/Game'
 import './style/normalize.css'
 import './style/index.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Game />
   );
 }
 
---
+++ src/components/Game.tsx
@@ -0,0 +1,32 @@
+import vampire from '../assets/vampire.svg';
+
+const Game = () => {
+  function update(e: any) {
+  var x = e.clientX || e.touches[0].clientX;
+  var y = e.clientY || e.touches[0].clientY;
+
+  document.documentElement.style.setProperty("--cursorX", x + "px");
+  document.documentElement.style.setProperty("--cursorY", y + "px");
+}
+
+  document.addEventListener("mousemove", update);
+  document.addEventListener("touchmove", update);
+  
+  return(
+    <main className="main">
+      <h1 className="main__text">
+        Shine your light fully on the monster hidden in the dark before it eats
+        you!
+      </h1>
+      <p className="main__text">Note: This game will have audio.</p>
+      <img className="main__monster" src={ vampire } alt="monster" />
+      <div className="main__buttons">
+        <button className="button button__hard">Hard</button>
+        <button className="button button__medium">Medium</button>
+        <button className="button button__easy">Easy</button>
+      </div>
+    </main>
+  )
+}
+
+export default Game;
