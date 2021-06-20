7b343a1cf8333fcfcd8229e5bd9cb0eef26eb0da
7redeagle <25111698+7redeagle@users.noreply.github.com>
2021-06-19 21:48:32 -0400

    added framer motion

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,33 +1,29 @@
-import vampire from '../assets/vampire.svg';
+import monster from '../assets/monster.svg';
 
 const Game = () => {
-  
-  // cursor flashlight
-  function update(e: any) {
-  var x = e.clientX || e.touches[0].clientX;
-  var y = e.clientY || e.touches[0].clientY;
-
-  document.documentElement.style.setProperty("--cursorX", x + "px");
-  document.documentElement.style.setProperty("--cursorY", y + "px");
+  // Onclick game screen disappears 
+  function playGame() {
+    alert('hello')
   }
 
-  document.addEventListener("mousemove", update);
-  document.addEventListener("touchmove", update);
-
-  // game on click buttons
-  
-  return(
+  return (
     <main className="main">
       <h1 className="main__text">
         Shine your light fully on the monster hidden in the dark before it eats
         you!
       </h1>
       <p className="main__text">Note: This game will have audio.</p>
-      <img className="main__monster" src={ vampire } alt="monster" />
+      <img className="main__monster" src={ monster } alt="monster" />
       <div className="main__buttons">
-        <button className="button button__hard">Hard</button>
+        <button className="button button__hard" onClick={playGame}>Hard</button>
         <button className="button button__medium">Medium</button>
         <button className="button button__easy">Easy</button>
+      </div>
+      <div className="footer">
+        <p className="footer__copyright">
+          @Made by <a href="https://github.com/7redeagle/">7redeagle</a> |
+          <a className="popup__link" href="#popup">Attributions</a>.
+        </p>
       </div>
     </main>
   )
